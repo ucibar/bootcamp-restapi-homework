e4f36991d42792383e4711cd01a11441bf94190e
Ugur Cibaroglu <ugur.cibar@gmail.com>
2021-12-12 11:30:06 +0300

    go fmt

--- model/order.go
+++ model/order.go
@@ -3,12 +3,12 @@ package model
 import "time"
 
 type Order struct {
-	ID int
-	Items []*OrderItem `json:"items"`
-	CreatedAt time.Time `json:"created_at"`
+	ID        int
+	Items     []*OrderItem `json:"items"`
+	CreatedAt time.Time    `json:"created_at"`
 }
 
 type OrderItem struct {
-	BookID int `json:"book_id"`
+	BookID   int `json:"book_id"`
 	Quantity int `json:"quantity"`
 }
